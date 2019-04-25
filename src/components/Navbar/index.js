27df0823722bb51f5a6bf71c11c7622a0e4c9d76
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-04-25 08:29:31 -0400

    won message updated

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,7 +9,7 @@ function Navbar(props) {
             <span className="nav-text brand">
                 Clicky Game
                 </span>
-            <span className={props.message === ("You guessed correctly!" || "You won! Click an image to play again!") ? "nav-text font-italic text-success nav-message" : (props.message === "Game over. Click an image to start again." ? "nav-text redText nav-message" : "nav-text nav-message")}>
+            <span className={props.message === "You guessed correctly!" ? "nav-text font-italic text-success nav-message" : (props.message === "Game over. Click an image to start again." ? "nav-text redText nav-message" : (props.message === "You won! Click an image to play again!" ? "nav-text font-weight-bolder text-warning nav-message" : "nav-text nav-message"))}>
                 {props.message}
             </span>
             <span className="nav-text">
