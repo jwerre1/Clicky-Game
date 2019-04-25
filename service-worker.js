a1c5ff444efc5d240440af8695798527df325b40
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-04-25 12:26:28 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98f8a3d69e99c41bcb2620c90322f472.js"
+  "/precache-manifest.36fbbf5c8a0ce938d3c95b3d78551b1f.js"
 );
 
 self.addEventListener('message', (event) => {
